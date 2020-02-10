f3c332a8a5bb0ac5290809703923c1501535cd69
RizwanaParveen <rizwanaparveen64@gmail.com>
2020-02-10 16:31:11 +0530

    Update variables.tf

--- 04-Terraform/03-Ansible-Deploy/variables.tf
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -13,3 +13,25 @@ variable "us-east-zones" {
 variable "sg-id" {
   default = "sg-0be7e9e40082f94b5"
 }
+variable "ami_name" {
+    default = "ubuntu"
+}
+variable "ami_id" {
+    default = "ami-0994c095691a46fb5"
+}
+variable "ami_key_pair_name" {
+    default = "test-key"
+}
+variable "region" {
+    default = "us-west-2"
+}
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "zone" {
+    default = "us-west-2a"
+}
+variable "port"{
+    default = 22
+}
